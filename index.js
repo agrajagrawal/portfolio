4fa8742c5acbafdc24c54e086de587583c68d006
Aman Kumar Singh <maskmanaman@gmail.com>
2021-06-16 14:25:24 +0530

    navbar finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// theme change implementation
 const themeMap = {
     dark: "light",
     light: "solar",
@@ -20,3 +21,21 @@ $("#themeButton").click(function(e){
     e.preventDefault();
     toggleTheme();
 });
+
+// navbar handling
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar-nav");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
